9dcba6b86a147237f404eecb7dd60c973f047818
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-15 22:41:11 +0100

    Update Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,15 @@
 import styles from "./Home.module.css";
+import TransactionForm from "../../components/TransactionForm";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}> Transaction List</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
