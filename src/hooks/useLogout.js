70641266d79053a31ebea575cc4ab91cd7586d56
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-15 19:38:14 +0100

    Update useLogout.js

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { moneytorAuth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogout = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const logout = async () => {
+    setError(null);
+    setIsPending(true);
+
+    //sign the user out
+    try {
+      await moneytorAuth.signOut();
+
+      //dispatch log out action
+      dispatch({ type: "LOGOUT" });
+
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
+    } catch (err) {
+      if (!isCancelled) {
+        setError(err.message);
+        setIsPending(false);
+      }
+    }
+
+    console.log("cancel: ", isCancelled);
+  };
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { logout, error, isPending };
+};
