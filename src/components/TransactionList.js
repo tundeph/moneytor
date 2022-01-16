6c977b53f97e08c4e37c45bbeaa1ecaed93b7dc3
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-17 00:42:33 +0100

    Create TransactionList.js

---
+++ src/components/TransactionList.js
@@ -0,0 +1,23 @@
+import { useFirestore } from "../hooks/useFirestore"
+
+//styles
+import styles from "./TransactionList.module.css"
+
+const TransactionList = ({ transactions }) => {
+  const { deleteDocument, response } = useFirestore()
+  console.log(response)
+
+  return (
+    <ul className={styles.transactions}>
+      {transactions.map((transaction) => (
+        <li key={transaction.id}>
+          <p className={styles.name}>{transaction.name}</p>
+          <p className={styles.amount}>${transaction.amount}</p>
+          <button onClick={() => deleteDocument(transaction.id)}> x</button>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default TransactionList
