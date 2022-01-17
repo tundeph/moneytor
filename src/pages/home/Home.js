ef5b99fe9cd28110676ec5790350d29100f7320f
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-17 14:11:08 +0100

    Update Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,32 @@
-import styles from "./Home.module.css";
-import TransactionForm from "../../components/TransactionForm";
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { useCollection } from "../../hooks/useCollection"
+
+//components
+import TransactionForm from "../../components/TransactionForm"
+import TransactionList from "../../components/TransactionList"
+
+//styles
+import styles from "./Home.module.css"
 
 const Home = () => {
+  const { user } = useAuthContext()
+  const { documents, error } = useCollection(
+    "transactions",
+    ["uid", "==", user.uid],
+    ["createdAt", "desc"]
+  )
+
   return (
     <div className={styles.container}>
-      <div className={styles.content}> Transaction List</div>
+      <div className={styles.content}>
+        {error && <p>{error} </p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
-        <TransactionForm />
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
