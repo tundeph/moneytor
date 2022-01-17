88b4a0faf69425d3bd3259f68bba85fc4c80dcf5
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-17 11:52:58 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/auth"
 // Import the functions you need from the SDKs you need
 //import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -14,13 +14,16 @@ const firebaseConfig = {
   storageBucket: "moneytor-822f2.appspot.com",
   messagingSenderId: "814379692812",
   appId: "1:814379692812:web:7673af7264834c08eecf29",
-};
+}
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
 //initalize services
-const moneytorAuth = firebase.auth();
-const moneytorDb = firebase.firestore();
+const moneytorAuth = firebase.auth()
+const moneytorDb = firebase.firestore()
 
-export { moneytorDb, moneytorAuth };
+//timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { moneytorDb, moneytorAuth, timestamp }
