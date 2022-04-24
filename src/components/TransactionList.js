86b174913435a514c765b1071ce76398196f6d71
tundeph <54335828+tundeph@users.noreply.github.com>
2022-04-24 22:12:04 +0100

    Update TransactionList.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -12,7 +12,7 @@ const TransactionList = ({ transactions }) => {
       {transactions.map((transaction) => (
         <li key={transaction.id}>
           <p className={styles.name}>{transaction.name}</p>
-          <p className={styles.amount}>${transaction.amount}</p>
+          <p className={styles.amount}>£{transaction.amount}</p>
           <button onClick={() => deleteDocument(transaction.id)}> x</button>
         </li>
       ))}
